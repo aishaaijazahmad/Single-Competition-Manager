d6d66d03f9689954a17f55541d056023be635c7d
Aisha Aijaz Ahmad <aishaaijazahmad@gmail.com>
2018-12-16 21:02:39 +0530

    Create Aisha_Main.java

---
+++ Aisha_Main.java
@@ -0,0 +1,24 @@
+/**
+* Software Engineering Foundations Coursework - 1
+* This is the main class of the project.
+* Dated: 23/10/2018
+* @author Aisha Aijaz Ahmad
+* @version 1.0
+*/
+
+public class Aisha_Main {
+
+/////////////////////////////////////////////////////////////////////////////////
+	
+	/**This is the main function. It initializes an object from Aisha's Competitor Manager class and calls the method run().
+	 * @param args This is the default parameter invoked by the main function.
+	 */
+	public static void main(String[] args) 
+	{
+		Aisha_CompetitorManager cm = new Aisha_CompetitorManager();
+		cm.run();
+	}
+
+/////////////////////////////////////////////////////////////////////////////////
+
+}
